33953f5e52a15cc4c1372be768c11ce9fb2ecb63
sattwikroy570 <sattwikroy@hotmail.com>
2022-07-09 12:04:55 +0530

    try

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace RulesMicroservice
 {
@@ -15,8 +14,6 @@ namespace RulesMicroservice
     {
         public static void Main(string[] args)
         {
-            var log4netRepository = log4net.LogManager.GetRepository(Assembly.GetEntryAssembly());
-            log4net.Config.XmlConfigurator.Configure(log4netRepository, new FileInfo("log4net.config"));
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -24,12 +21,7 @@ namespace RulesMicroservice
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>()
-                    .ConfigureLogging((hostingContext, logging) =>
-                     {
-                         logging.AddLog4Net();
-                         logging.SetMinimumLevel(LogLevel.Debug);
-                     });
+                    webBuilder.UseStartup<Startup>();
                 });
     }
 }
